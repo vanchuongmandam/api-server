53f6359e6ac0dc360372c11002a9dc8db117a0ec
longathelstan <121792617+longathelstan@users.noreply.github.com>
2025-08-03 22:13:45 +0700

    more

--- src/models/article.model.js
+++ src/models/article.model.js
@@ -5,11 +5,12 @@ const articleSchema = new mongoose.Schema({
     slug: { type: String, required: true, unique: true },
     title: { type: String, required: true },
     author: { type: String, required: true },
-    date: { type: Date, default: Date.now },
+    date: { type: String, required: true }, 
     category: { type: String, required: true },
     excerpt: { type: String },
     content: { type: String, required: true },
-    imageUrl: { type: String }
+    imageUrl: { type: String },
+    trending: { type: Boolean, default: false }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Article', articleSchema);
