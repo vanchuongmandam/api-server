01c56aae9d3a2821c076e371ae681564fb643b6a
longaluminium10 <longaluminium10@gmail.com>
2025-08-24 13:27:03 +0000

    :)

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use('/media', express.static(process.env.UPLOAD_PATH));
 
 // --- Routes ---
 const authRoutes = require("./src/routes/auth.routes");
